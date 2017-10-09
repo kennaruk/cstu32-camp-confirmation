9012b9c2a6910c7f6ab7b0fa6837cc5ee7fb2da7
Nattanon Yanil <nattanonyanil@gmail.com>
2017-10-09 17:48:52 +0700

    Mock up 3 pages client

--- routes/client.js
+++ routes/client.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('client/index.ejs', { title: 'Express' });
+  res.render('client/index.ejs');
 });
+router.get('/getPage1', function(req, res, next) {
+  res.render('client/page1.ejs');
+});
+router.get('/getPage2', function(req, res, next) {
+  res.render('client/page2.ejs');
+});router.get('/getPage3', function(req, res, next) {
+  res.render('client/page3.ejs');
+});
+
 
 module.exports = router;

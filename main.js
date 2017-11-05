651e259befe52e9c726ae1e95e714a11c9db4aa9
Tanakornl3oom <thanakorn_l3oomlnw@hotmail.com>
2017-11-05 23:30:54 +0700

    change range (A:M) index is 13

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function getData(auth) {
 // const sheet = 'Sheet1!';
 const spreadsheetId = '1I9BvHPQlxsIAVxBZWXqAry0Fdu0tePcttAiJ2gR8FRY';
 const sheet = 'Testing!';
-const range = sheet+'A2:K';
+const range = sheet+'A2:M';
 const sheets = google.sheets('v4');
 
 exports.getDataAdmin = (callback) =>  {
